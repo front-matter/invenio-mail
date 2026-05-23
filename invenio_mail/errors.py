3ee024430598ea99b8c7e6c96ce3df59a11410cf
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-05-23 21:17:09 +0200

    chore(licenses): update license headers to use SPDX

--- invenio_mail/errors.py
+++ invenio_mail/errors.py
@@ -1,10 +1,5 @@
-# -*- coding: utf-8 -*-
-#
-# This file is part of Invenio.
-# Copyright (C) 2023      University of Münster.
-#
-# Invenio is free software; you can redistribute it and/or modify it
-# under the terms of the MIT License; see LICENSE file for more details.
+# SPDX-FileCopyrightText: 2023 University of Münster.
+# SPDX-License-Identifier: MIT
 
 """Errors and exceptions for mail module."""
 
